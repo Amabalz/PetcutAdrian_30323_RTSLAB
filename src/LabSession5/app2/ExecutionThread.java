eea01316dc1224c0a27b7db07efcfa49c95d64f8
Petcut Adrian <iq2adi@gmail.com>
2024-06-03 14:02:02 +0300

    all labs

---
+++ src/LabSession5/app2/ExecutionThread.java
@@ -0,0 +1,105 @@
+package LabSession5.app2;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Lock;
+
+public class ExecutionThread extends Thread{
+    Lock l1,l2;
+    CountDownLatch c;
+    int sleep_time, activity_min, activity_max;
+    public ExecutionThread(Lock l1, Lock l2, CountDownLatch c,int sleep_time, int activity_min, int activity_max) {
+        this.l1 = l1;
+        this.l2 = l2;
+        this.c = c;
+        this.sleep_time = sleep_time;
+        this.activity_min = activity_min;
+        this.activity_max = activity_max;
+    }
+
+    public void run() {
+        System.out.println(this.getName() + " - STATE 1");
+
+        if(l1!=null)
+        {
+            this.l1.lock();
+            System.out.println(this.getName() + "STATE 2");
+
+            int k = (int)Math.round(Math.random()*(activity_max-activity_min)+activity_min);
+            for(int i = 0;i<k*100000;i++)
+            {
+                i++;
+                i--;
+            }
+            try {
+                Thread.sleep(sleep_time*1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            this.l1.unlock();
+            System.out.println(this.getName() + "STATE 3");
+
+
+        }
+        else
+            if(l1!= null && l2!=null)
+            {
+                this.l1.lock();
+                this.l2.lock();
+
+                System.out.println(this.getName() + "STATE 2");
+
+                int k = (int)Math.round(Math.random()*(activity_max-activity_min)+activity_min);
+                for(int i = 0;i<k*100000;i++)
+                {
+                    i++;
+                    i--;
+                }
+                try {
+                    Thread.sleep(sleep_time*1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                this.l1.unlock();
+                this.l2.unlock();
+                System.out.println(this.getName() + "STATE 3");
+
+            }
+            else if( l2!=null)
+            {
+                {
+
+                    this.l2.lock();
+
+                    System.out.println(this.getName() + "STATE 2");
+
+                    int k = (int)Math.round(Math.random()*(activity_max-activity_min)+activity_min);
+                    for(int i = 0;i<k*100000;i++)
+                    {
+                        i++;
+                        i--;
+                    }
+
+                    try {
+                        Thread.sleep(sleep_time*1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    this.l2.unlock();
+                    System.out.println(this.getName() + "STATE 3");
+
+                }
+            }
+
+        this.c.countDown();
+        try {
+           this. c.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
---
+++ src/LabSession5/app2/Main.java
@@ -0,0 +1,29 @@
+package LabSession5.app2;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+    public static void main(String[] args) {
+        Lock l1 = new ReentrantLock();
+        Lock l2 = new ReentrantLock();
+
+        CountDownLatch c;
+
+        while (true) {
+            c = new CountDownLatch(4);
+
+            new ExecutionThread(l1, null, c, 4, 2, 4).start();
+            new ExecutionThread(l1, l2, c, 3, 3, 6).start();
+            new ExecutionThread(null, l2, c, 5, 2, 5).start();
+            c.countDown();
+            try {
+                c.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+}
